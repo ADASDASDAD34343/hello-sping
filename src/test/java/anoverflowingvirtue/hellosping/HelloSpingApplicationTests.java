ea9ed78b5e0f30c2f2e5c889bfc424880e591afa
ADASDASDAD34343 <107790054+ADASDASDAD34343@users.noreply.github.com>
2023-01-09 15:15:22 +0900

    Initial commit

---
+++ src/test/java/anoverflowingvirtue/hellosping/HelloSpingApplicationTests.java
@@ -0,0 +1,13 @@
+package anoverflowingvirtue.hellosping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
