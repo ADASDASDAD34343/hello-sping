de7e6f68025dea55e358b2a102ce9e98e6679b9a
ADASDASDAD34343 <107790054+ADASDASDAD34343@users.noreply.github.com>
2023-01-10 15:25:52 +0900

    HelloController.java 파일추가 오류 해결

--- src/main/java/anoverflowingvirtue/hellosping/controller/HelloController.java
+++ src/main/java/anoverflowingvirtue/hellosping/controller/HelloController.java
@@ -1,4 +1,17 @@
 package anoverflowingvirtue.hellosping.controller;
 
+import ch.qos.logback.core.model.Model;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.service.annotation.GetExchange;
+
+@Controller
 public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "Hello");
+
+        return "hello";
+    }
 }
